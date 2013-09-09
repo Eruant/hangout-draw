e4d26fe3f225668236a0d7f03c6b687e0ef3a0af
Matt Gale <matt@littleball.co.uk>
2013-09-09 11:18:15 +0100

    Adding drawing to hangout

--- v2/fragments/main.js
+++ v2/fragments/main.js
@@ -1,39 +1,113 @@
+window.requestAnimFrame = (function (callback) {
+    'use strict';
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+        window.setTimeout(callback, 1000 / 30);
+    };
+}());
+
 (function () {
 
     var app = {
+        
+        options: {
+            canvasID: 'drawArea',
+            width: 600,
+            height: 400
+		},
+        
+        pen: {
+            x: 0,
+            y: 0,
+            prevX: 0,
+            prevY: 0,
+            down: false
+        },
 
         init: function () {
-
-            console.log('Starting hangout');
-
-            gapi.hangout.data.submitDelta({
-                key1: 'first key',
-                key2: 'second key'
-            });
-
-            gapi.hangout.data.onStateChanged.add(function () {
-                var state = gapi.hangout.data.getState();
-                console.log(state);
-            });
-
+            
+            // set up the canvas
+            this.canvas = document.getElementById(this.options.canvasID);
+            this.canvas.width = this.options.width;
+            this.canvas.height = this.options.height;
+            
+            // set up the context
+            this.ctx = this.canvas.getContext('2d');
+            
             this.events();
+            this.loop();
+        },
+        
+        clear: function () {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        },
+        
+        mousePosition: function (ev) {
+            this.pen.prevX = ev.clientX - this.canvas.offsetLeft;
+            this.pen.prevY = ev.clientY - this.canvas.offsetTop;
+        },
+        mouseDown: function (ev) {
+            this.mousePosition(ev);
+            this.pen.down = true;
+            ev.preventDefault();
+        },
+        mouseUp: function (ev) {
+            this.pen.down = false;
+            ev.preventDefault();
+        },
+        mouseMove: function (ev) {
+            this.pen.x = ev.clientX - this.canvas.offsetLeft;
+            this.pen.y = ev.clientY - this.canvas.offsetTop;
+        },
+        
+        draw: function () {
+            
+            var pen = this.pen,
+                ctx = this.ctx;
 
+            if (pen.down) {
+                ctx.save();
+                ctx.beginPath();
+                if (pen.prevX > 0 && pen.prevY > 0) {
+                    ctx.moveTo(pen.prevX, pen.prevY);
+                }
+                ctx.lineTo(pen.x, pen.y);
+                ctx.closePath();
+                ctx.stroke();
+                ctx.restore();
+                
+                pen.prevX = pen.x;
+                pen.prevY = pen.y;
+            }
+        },
+        
+        loop: function () {
+            this.update();
+            this.draw();
+            window.requestAnimationFrame(this.loop.bind(this));
         },
 
         events: function () {
 
-            var input = document.getElementById('input'),
-                btn = document.getElementById('submit'),
-                value = '';
+            var btn = document.getElementById('submit');
+            
+            this.canvas.addEventListener('mousedown', this.mouseDown.bind(this), false);
+            this.canvas.addEventListener('mouseup', this.mouseUp.bind(this), false);
+            this.canvas.addEventListener('mousemove', this.mouseMove.bind(this), false);
 
-            btn.onclick = function () {
+            btn.addEventListener('click', function () {
                 console.log('Button pressed');
-                value = input.value;
+                value = this.ctx.getImageData(0, 0, this.options.width, this.options.height);
 
                 gapi.hangout.data.submitDelta({
-                    inputData: value
+                    img: value
                 });
-            };
+            });
+            
+            // listen out for when the shared object changes
+            gapi.hangout.data.onStateChanged.add(function () {
+                var state = gapi.hangout.data.getState();
+                console.log(state);
+            });
 
         }
 
