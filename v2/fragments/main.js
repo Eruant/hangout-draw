9f3c3178a91f3246b654c903e32517a7972191b1
Matt Gale <matt@littleball.co.uk>
2013-09-09 10:41:48 +0100

    defered js until Google is ready

--- v2/fragments/main.js
+++ v2/fragments/main.js
@@ -1,15 +1,28 @@
-(function (window, document) {
+(function () {
 
-    var results = document.getElementById('results');
+    var app = {
 
-    gapi.hangout.data.submitDelta({
-        key1: 'first key',
-        key2: 'second key'
+        init: function () {
+
+            gapi.hangout.data.submitDelta({
+                key1: 'first key',
+                key2: 'second key'
+            });
+
+            gapi.hangout.data.onStateChanged.add(function () {
+                var state = gapi.hangout.data.getState();
+                console.log(state);
+            });
+
+        }
+
+    };
+
+    gapi.hangout.onApiReady.add(function (eventObj) {
+        if (eventObj.isApiReady) {
+            app.init();
+        }
     });
 
-    gapi.hangout.data.onStateChanged.add(function (event) {
-        var state = gapi.hangout.data.getState();
-        console.log(state);
-    });
 
-}(window, window.document));
+}());
